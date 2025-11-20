67ec38886d27d671d45d18365bcba5c6af91ce31
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-11-20 17:46:09 +0100

    vip_ip

--- src/server/guards/is-authorized.guard.js
+++ src/server/guards/is-authorized.guard.js
@@ -3,6 +3,13 @@ const jwt = require('jsonwebtoken')
 const { getSecretKey } = require('../guards/getCredentials')
 
 module.exports = function (request, _reply, done) {
+  const allowedIps = (process.env.VIP_API_ALLOWED_IPS || '').split(',').map(ip => ip.trim())
+  const clientIp = request.ip
+
+  if (allowedIps.includes(clientIp)) {
+    return done()
+  }
+
   const secretKey = getSecretKey()
   const data = jwt.verify(request.auth.token, secretKey)
   if (!data.clientId) {
