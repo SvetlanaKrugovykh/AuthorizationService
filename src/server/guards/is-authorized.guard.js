328c9312dcc144b855047019a035f1a28048896e
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-11-21 18:12:59 +0100

    ::ffff:

--- src/server/guards/is-authorized.guard.js
+++ src/server/guards/is-authorized.guard.js
@@ -3,9 +3,14 @@ const jwt = require('jsonwebtoken')
 const { getSecretKey } = require('../guards/getCredentials')
 
 module.exports = function (request, _reply, done) {
-  const allowedIps = (process.env.VIP_API_ALLOWED_IPS || '').split(',').map(ip => ip.trim())
-  const clientIp = request.ip
 
+  const allowedIps = (process.env.VIP_API_ALLOWED_IPS || '').split(',').map(ip => ip.trim())
+  let clientIp = request.ip
+  // Normalize IPv6-mapped IPv4 addresses
+  if (clientIp.startsWith('::ffff:')) {
+    clientIp = clientIp.replace('::ffff:', '')
+  }
+  console.log(`[is-authorized.guard] Client IP: ${clientIp}, Allowed IPs: ${allowedIps}`)
   if (allowedIps.includes(clientIp)) {
     return done()
   }
