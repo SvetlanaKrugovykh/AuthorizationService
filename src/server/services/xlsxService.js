b8e255c2a2448e963e39847ec5f5979286849f19
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-12-09 14:41:13 +0100

    213

--- src/server/services/xlsxService.js
+++ src/server/services/xlsxService.js
@@ -31,113 +31,11 @@ module.exports.doConvertXlsx = async function (inputFilePath) {
     hyperlinksMap[match[2]] = match[1]  // marker -> URL
   }
 
-  // Replace HYPERLINK formulas with just the display text
+  // MINIMAL TEST: Only replace HYPERLINK formulas, NO hyperlinks added
   content = content.replace(/=HYPERLINK\("([^"]+)","([^"]+)"\)/g, '$2')
   fs.writeFileSync(sharedStringsPath, content, 'utf8')
 
-  // Read existing relationships and find max ID
-  const relsDir = path.join(tempDir, 'xl', 'worksheets', '_rels')
-  fs.mkdirSync(relsDir, { recursive: true })
-  const relsPath = path.join(relsDir, 'sheet1.xml.rels')
-  let existingRels = ''
-  let maxId = 0
-
-  if (fs.existsSync(relsPath)) {
-    existingRels = fs.readFileSync(relsPath, 'utf8')
-    const idMatches = existingRels.match(/Id="rId(\d+)"/g)
-    if (idMatches) {
-      idMatches.forEach(match => {
-        const num = parseInt(match.replace(/\D/g, ''))
-        if (num > maxId) maxId = num
-      })
-    }
-  }
-
-  // Create relationship entries for all hyperlinks
-  let newRelsXml = ''
-  const hyperlinksToAdd = []
-  let nextId = maxId + 1
-
-  for (const [marker, url] of Object.entries(hyperlinksMap)) {
-    const relId = `rId${nextId}`
-    newRelsXml += `        <Relationship Id="${relId}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink" Target="${url}" TargetMode="External"/>\n`
-    hyperlinksToAdd.push({ marker, relId, url })
-    nextId++
-  }
-
-  // Update or create relationships file
-  let relsContent
-  if (existingRels) {
-    relsContent = existingRels.replace('</Relationships>', newRelsXml + '</Relationships>')
-  } else {
-    relsContent = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
-${newRelsXml}</Relationships>`
-  }
-  fs.writeFileSync(relsPath, relsContent, 'utf8')
-
-  // Find cells with hyperlink markers and build hyperlinks element
-  const worksheetPath = path.join(tempDir, 'xl', 'worksheets', 'sheet1.xml')
-  let worksheet = fs.readFileSync(worksheetPath, 'utf8')
-  // xmlns:r already exists in worksheet, no need to add
-
-  // Parse worksheet to find cells with markers and build hyperlinks
-  let hyperlinkElements = ''
-  
-  // For each marker, find cells that contain it
-  for (const { marker, relId } of hyperlinksToAdd) {
-    // Look for cells containing the marker text value
-    // The marker might be wrapped as: <c r="K14" ... t="s"><v>123</v></c> where shared string 123 contains the marker
-    // But we need to find which cells actually display the marker
-    // Simple approach: find all cells and check their content
-    
-    // Match pattern: <c r="CELLREF" ... >...<v>INDEX</v>...</c>
-    // Then check if that index in sharedStrings contains our marker
-    const cellPattern = /<c r="([A-Z]+\d+)"[^>]*>[\s\S]*?<v>(\d+)<\/v>[\s\S]*?<\/c>/g
-    let cellMatch
-    
-    while ((cellMatch = cellPattern.exec(worksheet)) !== null) {
-      const cellRef = cellMatch[1]
-      const stringIndex = cellMatch[2]
-      
-      // Check if this index in sharedStrings contains our marker
-      // Find the stringIndex-th <si> block
-      const siPattern = /<si[^>]*>[\s\S]*?<\/si>/g
-      let siMatch
-      let siCount = 0
-      
-      while ((siMatch = siPattern.exec(content)) !== null) {
-        if (siCount === parseInt(stringIndex)) {
-          // Found the matching <si> block
-          if (siMatch[0].includes(marker)) {
-            // This cell contains our marker!
-            hyperlinkElements += `          <hyperlink ref="${cellRef}" r:id="${relId}"/>\n`
-          }
-          break
-        }
-        siCount++
-      }
-    }
-  }
-
-  if (hyperlinkElements) {
-    const hyperlinks = `        <hyperlinks>\n${hyperlinkElements}        </hyperlinks>`
-    worksheet = worksheet.replace(/(<\/worksheet>)$/m, hyperlinks + '\n$1')
-  }
-
-  fs.writeFileSync(worksheetPath, worksheet, 'utf8')
-
-  // Update files inside existing archive (preserves original structure and MS Office compatibility)  
-  zip.updateFile('xl/sharedStrings.xml', Buffer.from(content, 'utf8'))
-  zip.updateFile('xl/worksheets/sheet1.xml', Buffer.from(worksheet, 'utf8'))
-  
-  // Add or update relationships file
-  const relsFileInZip = 'xl/worksheets/_rels/sheet1.xml.rels'
-  if (zip.getEntry(relsFileInZip)) {
-    zip.updateFile(relsFileInZip, Buffer.from(relsContent, 'utf8'))
-  } else {
-    zip.addFile(relsFileInZip, Buffer.from(relsContent, 'utf8'))
-  }
+  // Skip all hyperlinks logic for now
 
   zip.writeZip(outputFilePath)
 
