83bff3dad308120dcd60b7f18d73f963a4c19a39
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-12-09 14:53:38 +0100

    234

--- src/server/services/xlsxService.js
+++ src/server/services/xlsxService.js
@@ -35,96 +35,15 @@ module.exports.doConvertXlsx = async function (inputFilePath) {
   content = content.replace(/=HYPERLINK\("([^"]+)","([^"]+)"\)/g, '$2')
   fs.writeFileSync(sharedStringsPath, content, 'utf8')
 
-  // STEP 1: Add only relationships, no worksheet hyperlinks yet
-  const relsDir = path.join(tempDir, 'xl', 'worksheets', '_rels')
-  fs.mkdirSync(relsDir, { recursive: true })
-  const relsPath = path.join(relsDir, 'sheet1.xml.rels')
-  let existingRels = ''
-  let maxId = 0
-
-  if (fs.existsSync(relsPath)) {
-    existingRels = fs.readFileSync(relsPath, 'utf8')
-    const idMatches = existingRels.match(/Id="rId(\d+)"/g)
-    if (idMatches) {
-      idMatches.forEach(match => {
-        const num = parseInt(match.replace(/\D/g, ''))
-        if (num > maxId) maxId = num
-      })
-    }
-  }
-
-  let newRelsXml = ''
-  let nextId = maxId + 1
-  for (const [marker, url] of Object.entries(hyperlinksMap)) {
-    newRelsXml += `        <Relationship Id="rId${nextId}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink" Target="${url}" TargetMode="External"/>\n`
-    nextId++
-  }
-
-  if (newRelsXml) {
-    let relsContent
-    if (existingRels) {
-      relsContent = existingRels.replace('</Relationships>', newRelsXml + '</Relationships>')
-    } else {
-      relsContent = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
-${newRelsXml}</Relationships>`
-    }
-    fs.writeFileSync(relsPath, relsContent, 'utf8')
-
-    // Add relationships to ZIP
-    const relsFileInZip = 'xl/worksheets/_rels/sheet1.xml.rels'
-    if (zip.getEntry(relsFileInZip)) {
-      zip.updateFile(relsFileInZip, Buffer.from(relsContent, 'utf8'))
-    } else {
-      zip.addFile(relsFileInZip, Buffer.from(relsContent, 'utf8'))
-    }
-  }
-
-  // STEP 2: Add hyperlinks block to worksheet
-  const worksheetPath = path.join(tempDir, 'xl', 'worksheets', 'sheet1.xml')
-  let worksheet = fs.readFileSync(worksheetPath, 'utf8')
-
-  // Find cells with hyperlink markers and build hyperlinks element
-  let hyperlinkElements = ''
-  nextId = maxId + 1 // Reset nextId for hyperlinks
+  // SIMPLE APPROACH: Create inline HYPERLINK formulas in sharedStrings
+  // Don't touch relationships at all - just put back working HYPERLINK formulas
   
   for (const [marker, url] of Object.entries(hyperlinksMap)) {
-    const relId = `rId${nextId}`
-    
-    // Find cells containing the marker text
-    const cellPattern = /<c r="([A-Z]+\d+)"[^>]*>[\s\S]*?<v>(\d+)<\/v>[\s\S]*?<\/c>/g
-    let cellMatch
-    
-    while ((cellMatch = cellPattern.exec(worksheet)) !== null) {
-      const cellRef = cellMatch[1]
-      const stringIndex = cellMatch[2]
-      
-      // Check if this index in sharedStrings contains our marker
-      const siPattern = /<si[^>]*>[\s\S]*?<\/si>/g
-      let siMatch
-      let siCount = 0
-      
-      while ((siMatch = siPattern.exec(content)) !== null) {
-        if (siCount === parseInt(stringIndex)) {
-          if (siMatch[0].includes(marker)) {
-            hyperlinkElements += `          <hyperlink ref="${cellRef}" r:id="${relId}"/>\n`
-          }
-          break
-        }
-        siCount++
-      }
-    }
-    nextId++
+    const hyperlinkFormula = `=HYPERLINK("${url}","${marker}")`
+    content = content.replace(new RegExp(marker.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g'), hyperlinkFormula)
   }
-
-  // Add hyperlinks block if we found any
-  if (hyperlinkElements) {
-    const hyperlinks = `        <hyperlinks>\n${hyperlinkElements}        </hyperlinks>`
-    worksheet = worksheet.replace(/(<legacyDrawingHF[^>]*\/>)/, `$1\n${hyperlinks}`)
-  }
-
-  fs.writeFileSync(worksheetPath, worksheet, 'utf8')
-  zip.updateFile('xl/worksheets/sheet1.xml', Buffer.from(worksheet, 'utf8'))
+  
+  fs.writeFileSync(sharedStringsPath, content, 'utf8')
 
   zip.writeZip(outputFilePath)
 
